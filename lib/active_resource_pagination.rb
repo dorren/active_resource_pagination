1e8c7f929ad4719b61a640fbef2058bc6561a9fd
Dorren Chen <dorrenchen@gmail.com>
2010-06-02 13:21:12 -0400

    it works i think

---
+++ lib/active_resource_pagination.rb
@@ -0,0 +1,52 @@
+require 'active_resource'
+require 'will_paginate'
+
+module ActiveResource
+  # This adds pagination support to Active Resource. For example
+  #
+  #   Article.paginate
+  #   Article.paginate(:all, :page => 2, :per_page => 20)
+  #   Article.paginate(:all, :page => 2, :per_page => 20, :total_entries => 123)
+  #   Article.paginate(:all, :from => :most_popular,
+  #                    :params => {:year => 2010, :page => 1, :per_page => 20})
+  module Pagination
+    def self.included(base)
+      base.extend ClassMethods
+    end
+    
+    module ClassMethods
+      # can be overrided in subclass
+      def per_page
+        10
+      end
+      
+      # use same method signatures as find(), optional additional parameters: 
+      #   page - current page
+      #   per_pape - entries per page
+      #   total_entries - total entries count
+      #
+      # if resource backend doesn't paginate and retursn all result, then this method automatically
+      # sets the total_entry count from the result. Otherwise, you have to pass in the 
+      # :total_entries count value manually.
+      def paginate(*args)
+        options = args.last || {}
+        options = options[:params] || options
+        options = {:page => 1, :per_page => per_page}.merge(options)
+        page = options[:page]
+        per_page = options[:per_page]
+        
+        arr = find(*args)
+        total_entries = options[:total_entries] || arr.size 
+        WillPaginate::Collection.create(page, per_page, total_entries) do |pager|
+          if arr.size > per_page
+            pager.replace arr[pager.offset, pager.per_page]
+          else
+            pager.replace arr
+          end
+        end
+      end
+    end
+  end
+end
+
+ActiveResource::Base.send :include, ActiveResource::Pagination
--- spec/active_resource_pagination_spec.rb
+++ spec/active_resource_pagination_spec.rb
@@ -1,7 +1,65 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "ActiveResourcePagination" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe ActiveResource::Pagination do
+  describe "##paginate" do
+    before(:all) do
+      class Article < ActiveResource::Base
+        self.site = ''
+        
+        def self.per_page
+          2
+        end
+        
+        def self.from_xml(xml)
+          instantiate_collection(format.decode(xml))
+        end
+      end
+
+      # generate 5 articles, per_page is 2
+      xml = (1..5).collect{|x|  {:name => "article #{x}", :content => "blah blah #{x}"}}.to_xml(:root => "articles")
+      @articles = Article.from_xml(xml)
+    end
+    
+    it "should respond to paginate" do
+      ActiveResource::Base.should respond_to(:paginate)
+    end
+    
+    it "should override per_page" do
+      Article.per_page.should == 2
+    end
+
+    describe "when backend does not paginate" do  
+      before(:each) do
+        Article.should_receive(:find).and_return(@articles)      
+      end
+          
+      it "should paginate with no params" do
+        col = Article.paginate
+        col.current_page.should == 1
+        col.per_page.should == Article.per_page
+        col.total_entries.should == @articles.size
+        col.total_pages.should == 3  # (col.total_entries / col.per_page.to_f).ceil
+      end
+      
+      it "should paginate with params" do
+        col = Article.paginate(:page => 2)
+        col.current_page.should == 2
+        col.first.should == @articles[3]
+      end
+    end
+
+    describe "when backend do paginate" do
+      it "should paginate when backend returns paginated entries" do
+        @articles = @articles[2,2]  # returns 2nd page result
+        Article.should_receive(:find).and_return(@articles)
+        
+        col = Article.paginate(:page => 2, :total_entries => 5)
+        col.current_page.should == 2
+        col.per_page.should == Article.per_page
+        col.total_entries.should == 5
+        col.total_pages.should == 3  # (col.total_entries / col.per_page.to_f).ceil
+        col.first.should == @articles.first
+      end
+    end
   end
 end
